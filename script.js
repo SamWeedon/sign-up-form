e6f08b86ba73858ead88cba31cf607f9d7aefaf2
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-09-20 17:17:13 -0600

    Add logic for error message population

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let pass1 = document.querySelector('#user_password');
 let pass2 = document.querySelector('#password_confirmation');
 let form = document.querySelector('form');
+let errorMessage = document.querySelector('.error-message-placeholder');
 
 function checkPassword(event) {
     if (pass1.value !== pass2.value) {
         pass1.classList.add('error');
         pass2.classList.add('error');
+        errorMessage.textContent = '* Passwords do not match'
         event.preventDefault(); // prevents form submission
     }
     else {
         pass1.classList.remove('error');
         pass2.classList.remove('error');
+        errorMessage.textContent = '';
     }
 }
 
