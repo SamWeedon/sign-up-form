1602c76e2e81a2537d251a0f28224f64b6b42f4e
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-09-19 21:33:25 -0600

    Begin to build form validation

---
+++ script.js
@@ -0,0 +1,8 @@
+let pass1 = document.querySelector('#user_password')
+let pass2 = document.querySelector('#password_confirmation')
+
+function checkPassword() {
+    if (pass1.value == pass2.value) {
+
+    }
+}
