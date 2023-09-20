944cb112cc633b1419bda828f7c33a711b2f9e88
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-09-20 17:06:49 -0600

    Add logic for password confirmation

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let pass1 = document.querySelector('#user_password')
-let pass2 = document.querySelector('#password_confirmation')
+let pass1 = document.querySelector('#user_password');
+let pass2 = document.querySelector('#password_confirmation');
+let form = document.querySelector('form');
 
-function checkPassword() {
-    if (pass1.value == pass2.value) {
-
+function checkPassword(event) {
+    if (pass1.value !== pass2.value) {
+        pass1.classList.add('error');
+        pass2.classList.add('error');
+        event.preventDefault(); // prevents form submission
+    }
+    else {
+        pass1.classList.remove('error');
+        pass2.classList.remove('error');
     }
 }
+
+form.addEventListener('submit', checkPassword);
